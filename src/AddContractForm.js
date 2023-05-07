d8324d7e9c458e49367ae2b9484aa52182c90594
SuhasPalani <sugipals007@gmail.com>
2023-05-07 13:50:36 +0530

    update

--- src/AddContractForm.js
+++ src/AddContractForm.js
@@ -1,3 +1,51 @@
+// import React, { useState } from 'react';
+// import './AddContractForm.css';
+
+// function AddContractForm({ onAddContract }) {
+//   const [newServiceName, setNewServiceName] = useState('');
+//   const [newServiceDescription, setNewServiceDescription] = useState('');
+
+//   const handleSubmit = e => {
+//     e.preventDefault();
+//     onAddContract(newServiceName, newServiceDescription);
+//     setNewServiceName('');
+//     setNewServiceDescription('');
+//   };
+
+//   return (
+//     <form className="add-contract-form" onSubmit={handleSubmit}>
+//     <div className="dashboard">
+//       <h1>Comparator Testing Dashboard</h1>
+//       <div className="form-row">
+//         <div className="form-group">
+//           <label htmlFor="new-service-name">Service Name:</label>
+//           <input
+//             type="text"
+//             id="new-service-name"
+//             className="form-input"
+//             value={newServiceName}
+//             onChange={e => setNewServiceName(e.target.value)}
+//           />
+//         </div>
+//         <div className="form-group">
+//           <label htmlFor="new-service-description">Service Description:</label>
+//           <input
+//             type="text"
+//             id="new-service-description"
+//             className="form-input"
+//             value={newServiceDescription}
+//             onChange={e => setNewServiceDescription(e.target.value)}
+//           />
+//         </div>
+//       </div>
+//       <button type="submit" className="form-submit-button">Add Contract</button>
+//       </div>
+//     </form>
+//   );
+// }
+
+// export default AddContractForm;
+
 import React, { useState } from 'react';
 import './AddContractForm.css';
 
@@ -14,11 +62,10 @@ function AddContractForm({ onAddContract }) {
 
   return (
     <form className="add-contract-form" onSubmit={handleSubmit}>
-    <div className="dashboard">
-      <h1>Comparator Testing Dashboard</h1>
+      <h1 className="add-contract-heading">Compare Contracts</h1>
       <div className="form-row">
         <div className="form-group">
-          <label htmlFor="new-service-name">Service Name:</label>
+          <label htmlFor="new-service-name" className="form-label">Service Name:</label>
           <input
             type="text"
             id="new-service-name"
@@ -28,7 +75,7 @@ function AddContractForm({ onAddContract }) {
           />
         </div>
         <div className="form-group">
-          <label htmlFor="new-service-description">Service Description:</label>
+          <label htmlFor="new-service-description" className="form-label">Service Description:</label>
           <input
             type="text"
             id="new-service-description"
@@ -38,10 +85,11 @@ function AddContractForm({ onAddContract }) {
           />
         </div>
       </div>
-      <button type="submit" className="form-submit-button">Add Contract</button>
-      </div>
+      <button type="submit" className="form-submit-button">Compare Contracts</button>
     </form>
   );
 }
 
 export default AddContractForm;
+
+
--- src/ContractDashboard.jsx
+++ src/ContractDashboard.jsx
@@ -1,14 +1,101 @@
+// import React, { useState, useEffect } from 'react';
+// import './ContractDashboard.css';
+// import { Link } from 'react-router-dom';
+
+
+
+
+// function ContractDashboard() {
+//   const [contracts, setContracts] = useState([]);
+//   const [newServiceName, setNewServiceName] = useState('');
+//   const [newServiceLocation, setNewServiceLocation] = useState('');
+
+//   // Fetch contracts from API
+//   useEffect(() => {
+//     fetch('/api/contracts')
+//       .then(response => response.json())
+//       .then(data => setContracts(data.contracts))
+//       .catch(error => console.error(error));
+//   }, []);
+
+//   // Handler for adding a new contract
+//   const handleAddContract = () => {
+//     const newContract = {
+//       id: contracts.length + 1,
+//       serviceName: newServiceName,
+//       location: newServiceLocation,
+      
+//     };
+//     setContracts([...contracts, newContract]);
+//     setNewServiceName('');
+//     setNewServiceLocation('');
+//   };
+
+//   return (
+//     <div className="dashboard">
+//       <h1>Contract Testing Dashboard</h1>
+//       <div className="dashboard-form">
+//         <label htmlFor="new-service-name">Service Name:</label>
+//         <input
+//           type="text"
+//           id="new-service-name"
+//           value={newServiceName}
+//           onChange={e => setNewServiceName(e.target.value)}
+//         />
+//         <label htmlFor="new-service-location">Service Location:</label>
+//         <input
+//           type="text"
+//           id="new-service-location"
+//           value={newServiceLocation}
+//           onChange={e => setNewServiceLocation(e.target.value)}
+//         />
+//         <button onClick={handleAddContract}>Download Contract</button>
+//       </div>
+//       <table className="dashboard-table">
+//         <thead>
+//           <tr>
+//             <th>Contract ID</th>
+//             <th>Service Name</th>
+//             <th>Service Location</th>
+            
+//           </tr>
+//         </thead>
+//         <tbody>
+//           {contracts.map(contract => (
+//             <tr key={contract.id}>
+//               <td>{contract.id}</td>
+//               <td>{contract.serviceName}</td>
+//               <td>{contract.location}</td>
+              
+//             </tr>
+//           ))}
+//         </tbody>
+//       </table>
+      
+//       <div className="dashboard-header">
+//         <button className="add-contract-button">
+//           <Link to="/add-contract">Add Contract</Link>
+//         </button>
+//       </div>
+
+//     </div>
+//   );
+// }
+
+// export default ContractDashboard;
+
+// ContractDashboard.js
+
 import React, { useState, useEffect } from 'react';
 import './ContractDashboard.css';
 import { Link } from 'react-router-dom';
 
 
 
-
 function ContractDashboard() {
   const [contracts, setContracts] = useState([]);
   const [newServiceName, setNewServiceName] = useState('');
-  const [newServiceLocation, setNewServiceLocation] = useState('');
+  const [newServiceDescription, setNewServiceDescription] = useState('');
 
   // Fetch contracts from API
   useEffect(() => {
@@ -19,16 +106,15 @@ function ContractDashboard() {
   }, []);
 
   // Handler for adding a new contract
-  const handleAddContract = () => {
+  const handleAddContract = (serviceName, serviceDescription) => {
     const newContract = {
       id: contracts.length + 1,
-      serviceName: newServiceName,
-      location: newServiceLocation,
-      
+      serviceName,
+      serviceDescription,
     };
     setContracts([...contracts, newContract]);
     setNewServiceName('');
-    setNewServiceLocation('');
+    setNewServiceDescription('');
   };
 
   return (
@@ -42,22 +128,23 @@ function ContractDashboard() {
           value={newServiceName}
           onChange={e => setNewServiceName(e.target.value)}
         />
-        <label htmlFor="new-service-location">Service Location:</label>
+        <label htmlFor="new-service-description">Service Description:</label>
         <input
           type="text"
-          id="new-service-location"
-          value={newServiceLocation}
-          onChange={e => setNewServiceLocation(e.target.value)}
+          id="new-service-description"
+          value={newServiceDescription}
+          onChange={e => setNewServiceDescription(e.target.value)}
         />
-        <button onClick={handleAddContract}>Download Contract</button>
+        <button onClick={() => handleAddContract(newServiceName, newServiceDescription)}>
+          Download Contract
+        </button>
       </div>
       <table className="dashboard-table">
         <thead>
           <tr>
             <th>Contract ID</th>
             <th>Service Name</th>
-            <th>Service Location</th>
-            
+            <th>Service Description</th>
           </tr>
         </thead>
         <tbody>
@@ -65,22 +152,20 @@ function ContractDashboard() {
             <tr key={contract.id}>
               <td>{contract.id}</td>
               <td>{contract.serviceName}</td>
-              <td>{contract.location}</td>
-              
+              <td>{contract.serviceDescription}</td>
             </tr>
           ))}
         </tbody>
       </table>
-      
       <div className="dashboard-header">
         <button className="add-contract-button">
           <Link to="/add-contract">Add Contract</Link>
         </button>
       </div>
+     
 
     </div>
   );
 }
 
 export default ContractDashboard;
-
